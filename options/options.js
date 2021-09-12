7baefcf1a57fe667459a632ee9c34a8deb8863ed
Crowdbotics <team@crowdbotics.com>
2021-09-12 21:09:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "noisy_flower_30485Identifier",
+  url: "https://noisy_flower_30485Identifier.botics.co",
+  api: "https://noisy_flower_30485Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
